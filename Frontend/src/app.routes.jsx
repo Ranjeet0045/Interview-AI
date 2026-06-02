22142cf427228a7489b4c24a41339c343f0414c4
Ranjeet <ranjeet127ad@gmail.com>
2026-06-02 20:26:17 +0530

    fix: resolve casing issue for Protected component import

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import Register from "./features/auth/pages/Register";
 import Login from "./features/auth/pages/Login";
-import Protected from "./features/auth/components/protected.jsx";
+import Protected from "./features/auth/components/Protected.jsx";
 import Home from "./features/interview/pages/Home.jsx";
 import Interview from "./features/interview/pages/interview.jsx";
 import Profile from "./features/auth/pages/Profile.jsx";
